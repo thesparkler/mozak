3bb22b1cf33fd7b1ffb987828cac9e9fbeb60f20
Amitkumar Prajapati <pamit2897@gmail.com>
2022-02-10 13:02:34 +0530

    Write all the assets(color, stirngs, images) code in seperate file for code readability

---
+++ lib/constants/AppAssets.dart
@@ -0,0 +1,21 @@
+class AppAssets {
+  // images
+  static const ydsLogo = "assets/images/logo_yds.png";
+  static const yuvak = "assets/images/yuvak.png";
+  static const hariSwamiBgImage = "assets/images/hari_swami.png";
+  static const maleImgBlack = "assets/images/form_ui_assets/male_img_black.png";
+  static const maleImgWhite = "assets/images/form_ui_assets/male_img_white.png";
+  static const femaleImgBlack =
+      "assets/images/form_ui_assets/female_img_black.png";
+  static const femaleImgWhite =
+      "assets/images/form_ui_assets/female_img_white.png";
+  static const birthDateCalendar = "assets/images/form_ui_assets/calendar.png";
+  static const studentImgWhite =
+      "assets/images/form_ui_assets/student_img_white.png";
+  static const studentImgBlack =
+      "assets/images/form_ui_assets/student_img_black.png";
+  static const professionalImgWhite =
+      "assets/images/form_ui_assets/working_img_white.png";
+  static const professionalImgBlack =
+      "assets/images/form_ui_assets/working_img_black.png";
+}
---
+++ lib/constants/AppStrings.dart
@@ -0,0 +1,34 @@
+class AppStrings {
+
+  //SPLASH TEXTS
+  static const String splashScreenTextOne = "Jai Swaminarayan\n";
+  static const String splashScreenTextTwo = "Das na Das";
+
+  //LOGIN TEXTS
+  static const String passwordHintText = "Must have at least 6 characters";
+  static const String emailHintText = "bhulkoo@example.com";
+  static const String emailText = "Email";
+  static const String passwordText = "Password";
+  static const String loginBtnText = "Login";
+
+  // VALIDATION TEXT
+  static const String isEmailRequired = "Email is required";
+  static const String isPasswordRequired = "Password is required";
+  static const String passwordLengthErrorText = "Password should be more than 6 char long";
+  static const String emailErrorText = "Please enter valid email";
+  static const String validationSuccessText = "Success";
+
+
+  // LOGIN SCREEN WELCOME MSG TEXT
+  static const String helloText = "Hello";
+  static const String bhulkooText = "\tbhulkoo,\n";
+  static const String welcomeBackText = "Welcome Back";
+
+  // EXIT DIALOG TEXTS
+  static const String dialogTitle = "Are you sure?";
+  static const String dialogDescription = "Do you want to exit the app";
+  static const String cancelBtnText = "Cancel";
+  static const String exitBtnText = "Exit";
+
+
+}
