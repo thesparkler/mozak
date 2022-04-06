14cf5668fc776bbc7b27b48d385d1db97c3334d6
Amitkumar Prajapati <pamit2897@gmail.com>
2022-04-06 17:27:09 +0530

    Write a function which will capitalize the first letter of a String

--- lib/utils/app_tools.dart
+++ lib/utils/app_tools.dart
@@ -1,5 +1,17 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
+
+
+
 Color hexToColor(String code) {
   return Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+}
+
+String capitalizeFirstLetter(String text) {
+  final String firstLetter = text.trim().substring(0, 1).toUpperCase();
+  final remainingLetters = text.trim().substring(1);
+  String combinedText = firstLetter + remainingLetters;
+  return combinedText;
 }
