4dbc8fc3f51353788df1a77a378288dccb59a162
Amitkumar Prajapati <pamit2897@gmail.com>
2022-04-06 17:34:05 +0530

    main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mozak/constants/AppColors.dart';
 import 'package:mozak/screens/SplashScreen.dart';
+import 'package:mozak/screens/userform/UserForm.dart';
+import 'package:mozak/utils/app_tools.dart';
 
-void main() {
+
+ main()  {
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,9 +23,12 @@ class MyApp extends StatelessWidget {
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
-    return const MaterialApp(
+    return  MaterialApp(
+      theme: ThemeData(
+        disabledColor: hexToColor(AppColors.grey)
+      ),
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
