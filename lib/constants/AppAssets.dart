11e2d05303841c6e324b30854f19ed354d9af05c
Shubham Kishor Dathia <54986627+mrdathia@users.noreply.github.com>
2023-01-29 04:56:03 +0530

    All Set

--- lib/constants/AppAssets.dart
+++ lib/constants/AppAssets.dart
@@ -1,5 +1,5 @@
 class AppAssets {
-  static const hariprabodhamLogo = "assets/images/unnamed.png";
+  static const hariprabodhamLogo = "assets/images/shri_hari_prabodham.png";
   static const net = "assets/images/no_signal.png";
   static const yuvak = "assets/images/yuvak.png";
   static const hariSwamiBgImage = "assets/images/hari_swami.png";
