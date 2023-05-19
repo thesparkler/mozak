47b454775bf26b4328d7c03a3fd90c57d3501dbd
Gupta <gupta527@avaya.com>
2023-05-19 18:10:01 +0530

    Urvesh added GridView.builder

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,114 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:mozak/screens/weekly_forum_events.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: LayoutBuilder(
 
-        builder: (BuildContext context , BoxConstraints constraints) {
-          return Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all()
-                ),
-                height: constraints.maxHeight * 0.33,
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text('Youths'),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all()
-                ),
-                height: constraints.maxHeight * 0.33,
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.of(context).push(
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        WeeklyForumEvents()));
-                          },
-                          child: Text('Weekly Forum Events'),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text('Weekly Forum Attendance'),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all()
-                ),
-                height: constraints.maxHeight * 0.33,
-                child: Row(
-                  children: [
-                    Expanded(
+    void goto(String path)
+    {
+      Navigator.of(context).pushNamed('$path');
+    }
 
-                      flex: 2,
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: TextButton(
+    List<String> options = ['Youth list','Add Weeklyforum','Add Center','Add Group','Take Attendance','Add Team'];
 
-                          onPressed: () {},
-                          child: Text('Centers'),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text('Groups'),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          );
-
-        }
-      ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.builder(
+          itemCount: options.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 1,
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10
+          ),
+          itemBuilder: (context, index) {
+            return Card(
+                color: Colors.amber[100],
+                elevation: 5,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: TextButton(
+                    onPressed: ()=>goto(options[index]),
+                    child:Text('${options[index]}',style: TextStyle(color: Colors.blue[900])),
+                  ),
+                )
+            );
+          }),
     );
   }
 }
