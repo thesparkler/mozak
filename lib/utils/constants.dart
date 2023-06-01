c48944350d56c3e63fdfa48c08dfab286953505b
Gupta <gupta527@avaya.com>
2023-06-02 00:55:14 +0530

    merged with registerYouth branch

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 
 class Constants {
-  static String domain = "http://192.168.29.220:8080/mozakio";
-  // static String domain = "http://192.168.1.34:8080/mozakio";
-  static String allYouths = "/allYouths";
-  static String allWFEvents = "/allWFEvents";
-  static String allCenters = "/allCenters";
-  static String allGroups = "";
-  static String allTeams = "";
+  // static String domain="http://192.168.29.220:8080/mozakio";
+  static String domain="http://192.168.1.35:8080/mozakio";
+
+  static String allYouths="/allYouths";
+  static String allWFEvents="/allWFEvents";
+  static String allCenters="/allCenters";
+  static String allGroups="";
+  static String allTeams="";
 
   static String createCenter = "/createCenter";
   static String createWFEvent = "/createWFEvent";
