faf2ba1dd5e1ee1f6cd48422e4e3640edd23fba9
mrdathia <shubhamdathia7257@gmail.com>
2023-06-05 19:14:13 +0530

    YouthRegistration new UI - adding form fields

--- lib/screens/youthRegistration.dart
+++ lib/screens/youthRegistration.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../constants/AppColors.dart';
 import '../utils/api_service.dart';
 import '../utils/app_tools.dart';
 import 'package:email_validator/email_validator.dart';
+
+
+
+final Map<String, String> group = {
+    "Aksharbramh":"AB",
+    "Bramhadarshan": "BR",
+    "Charanraj": "CR",
+    "Dasatva": "DS",
+    "Gurukrupa": "GK",
+    "Harikrupa": "HK",
+    "Santkrupa": "SK",
+    "Sarvamangal": "SM",
+    "Samanvay": "SY",
+  };
 
 class YouthRegistration extends StatefulWidget {
   const YouthRegistration({Key? key}) : super(key: key);
@@ -12,6 +27,37 @@ class YouthRegistration extends StatefulWidget {
 }
 
 class _YouthRegistrationState extends State<YouthRegistration> {
+
+  String youthCode = group.values.first;
+  String dropdownValue = group.keys.first;
+
+  DateTime? pickedDate;
+  var datecontroller=TextEditingController(text: "");
+  var date;
+  var selected;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void pickDate(BuildContext context) async{
+    selected = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(), //get today's date
+                  firstDate:DateTime(1934), //DateTime.now() - not to allow to choose before today.
+                  lastDate: DateTime.now(),
+                );
+
+     if (selected != null) {
+      var formatter = DateFormat('yyyy-MM-dd');
+
+      setState(() {
+        date = datecontroller.text = formatter.format(selected);
+      });
+    }
+
+  }
 
   
   @override
@@ -117,20 +163,89 @@ class _YouthRegistrationState extends State<YouthRegistration> {
                       height: constraint.maxHeight * 0.1,
                       width: constraint.maxWidth *0.8,
                       padding: EdgeInsets.all(5.0),
-                      color: Colors.amber,
-                      alignment: Alignment.center,
                       child: TextFormField(
-                        decoration: InputDecoration(border: OutlineInputBorder()),
-                      ),
+                        key: ValueKey('Pincode'),
+                        decoration: InputDecoration(
+                          labelText: 'Pincode',
+                          hintText: 'Enter your pincode',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) => 
+                          (!RegExp(r'(^(?:[+0]9)?[0-9]{10}$)').hasMatch(value!) || value.length > 6)
+                          ? 'Invalid pincode'
+                          : null,
+                        keyboardType: TextInputType.phone,
+                      )
                     ),
                     Container(
                       height: constraint.maxHeight * 0.1,
                       width: constraint.maxWidth *0.8,
                       padding: EdgeInsets.all(5.0),
                       child: TextFormField(
-                        decoration: InputDecoration(border: OutlineInputBorder())
+                        controller: datecontroller,
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.calendar_month),
+                          labelText: 'DOB',
+                          border: OutlineInputBorder(),
+                        ),
+                        readOnly: true,
+                        onTap: ()=>pickDate(context),
+                      )
+                    ),
+                    Container(
+                      height: constraint.maxHeight * 0.09,
+                      width: constraint.maxWidth *0.8,
+                      padding: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        border: Border.all(
+                          color: hexToColor(AppColors.textFieldOutlineBorderColor),
+                          width: 1.0,
+                        ),
                       ),
-                    )
+                      child:DropdownButton<String>(
+                        underline: SizedBox(), 
+                        value: dropdownValue,
+                        icon: SizedBox.shrink(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            dropdownValue = value!;
+
+                          });
+                        },
+                        items:group.keys.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      )
+                    ),
+                    Container(
+                      height: constraint.maxHeight * 0.09,
+                      width: constraint.maxWidth *0.8,
+                      padding: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        border: Border.all(
+                          color: hexToColor(AppColors.textFieldOutlineBorderColor),
+                          width: 1.0,
+                        ),
+                      ),
+                      child:DropdownButton<String>(
+                        underline: SizedBox(), 
+                        value: dropdownValue,
+                        icon: SizedBox.shrink(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            dropdownValue = value!;
+
+                          });
+                        },
+                        items: 
+                      )
+                    ),
                   ],
                 );
               },)
@@ -141,3 +256,11 @@ class _YouthRegistrationState extends State<YouthRegistration> {
   }
 }
  
+/*
+group.keys.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+*/
